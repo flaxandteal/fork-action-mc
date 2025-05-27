0714ad0641a9c5554292b22f6b4d4890f5c767f3
Phil Weir <phil.weir@flaxandteal.co.uk>
2025-05-27 10:50:19 +0100

    fix: mc config unexpectedly removed https://github.com/minio/mc/issues/5206

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ MC_API_SIGNATURE=${MC_API_SIGNATURE:="S3v4"}
 MC_BUCKET_LOOKUP=${MC_BUCKET_LOOKUP:="auto"}
 
 # Set mc configuration
-mc config host add "$MC_ALIAS" "$MC_URL" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" --api "$MC_API_SIGNATURE" --lookup "$MC_BUCKET_LOOKUP"
+mc alias set "$MC_ALIAS" "$MC_URL" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" --api "$MC_API_SIGNATURE" --lookup "$MC_BUCKET_LOOKUP"
 
 # Execute mc by expanding passed params and passing them to mc itself
 mc $*
